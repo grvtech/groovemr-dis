54b6aaa8e067da5c286c27e24566d0d562a5c346
Radu Gabor <radu@grvtech.ca>
2018-07-26 13:45:54 -0400

    first commit

---
+++ src/main/java/com/grvtech/dis/GroovemrDisApplication.java
@@ -0,0 +1,12 @@
+package com.grvtech.dis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovemrDisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovemrDisApplication.class, args);
+	}
+}
