ecb6f9f590db106528066d03d02a499d930712f1
Radu Gabor <radu@grvtech.ca>
2018-09-24 16:13:13 -0400

    encrypt work

--- src/main/java/com/grvtech/dis/controller/LoginController.java
+++ src/main/java/com/grvtech/dis/controller/LoginController.java
@@ -1,62 +1,69 @@
 package com.grvtech.dis.controller;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grvtech.dis.model.MessageResponse;
 import com.grvtech.dis.model.User;
 import com.grvtech.dis.repository.UserRepository;
+import com.grvtech.dis.service.UserService;
+import com.grvtech.dis.util.HttpUtil;
 
 @RestController
 public class LoginController {
 
 	@Autowired
 	UserRepository repository;
+	@Autowired
+	UserService userService;
 
 	@RequestMapping(value = { "/", "/index.html" }, method = RequestMethod.GET)
 	public ModelAndView index() {
 		User user = repository.findById(2);
 		/*
-		
-		if (user.isEmpty()) {
-			System.out.println("-----------------------------------------");
-			System.out.println("The user 2 is NOT in memory db : NON");
-			System.out.println("-----------------------------------------");
+		 * 
+		 * if (user.isEmpty()) {
+		 * System.out.println("-----------------------------------------");
+		 * System.out.println("The user 2 is NOT in memory db : NON");
+		 * System.out.println("-----------------------------------------");
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * //user = restTemplate.getForObject("http://localhost:8080/user/2",
+		 * User.class); //repository.insert(user);
+		 * 
+		 * System.out.println("-----------------------------------------");
+		 * System.out.println("add user in memory db : yes");
+		 * System.out.println("-----------------------------------------");
+		 * System.out.println("-----------------------------------------");
+		 * System.out.println("next refresh shoud be ok");
+		 * System.out.println("-----------------------------------------");
+		 * 
+		 * } else { System.out.println("-----------------------------------------");
+		 * System.out.println("The user 2 is in memory db : YES");
+		 * System.out.println("-----------------------------------------"); }
+		 * 
+		 * // RestTemplate restTemplate = new RestTemplate(); // User user =
+		 * restTemplate.getForObject("http://localhost:8080/user/2", // User.class);
+		 */
 
-			
-			
-			
-			
-			//user = restTemplate.getForObject("http://localhost:8080/user/2", User.class);
-			//repository.insert(user);
-
-			System.out.println("-----------------------------------------");
-			System.out.println("add user in memory db : yes");
-			System.out.println("-----------------------------------------");
-			System.out.println("-----------------------------------------");
-			System.out.println("next refresh shoud be ok");
-			System.out.println("-----------------------------------------");
-
-		} else {
-			System.out.println("-----------------------------------------");
-			System.out.println("The user 2 is in memory db : YES");
-			System.out.println("-----------------------------------------");
-		}
-
-		// RestTemplate restTemplate = new RestTemplate();
-		// User user = restTemplate.getForObject("http://localhost:8080/user/2",
-		// User.class);
-	*/
-		
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("index");
 		modelAndView.addObject(user);
@@ -64,28 +71,104 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = { "/login" }, method = RequestMethod.POST)
-	public ModelAndView login(@RequestBody @Valid String username,@RequestBody @Valid String password,  BindingResult bindingResult, final HttpServletRequest request) {
+	public MessageResponse login(final HttpServletRequest request) {
 
 		// RestTemplate restTemplate = new RestTemplate();
 		// User user = restTemplate.getForObject("http://localhost:8080/login",
 		// User.class);
 
+		JsonNode jn = HttpUtil.getJSONFromPost(request);
 		System.out.println("-----------------------------------------");
-		System.out.println("username : "+request.getParameter("username"));
-		System.out.println("password : "+request.getParameter("password"));
+		System.out.println("This is LOGIN POST API");
 		System.out.println("-----------------------------------------");
-		
-		
-		
+		System.out.println("timestamp : " + jn.get("timestamp").toString());
+		System.out.println("elements : " + jn.get("elements").toString());
+
+		String state = jn.get("state").toString().replaceAll("\"", "");
+		MessageResponse mresp = new MessageResponse();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyMMddhhmmss");
+		System.out.println("----------------------------------------- state:" + state);
+
+		if (state.equals("enc")) {
+			String elements = jn.get("elements").toString().replaceAll("\"", "");
+			elements = new String(Base64.getDecoder().decode(elements));
+			// System.out.println(new String(Base64.getDecoder().decode(elements)));
+
+			ObjectMapper mapper = new ObjectMapper();
+			JsonFactory factory = mapper.getFactory();
+			JsonParser parser;
+			try {
+				parser = factory.createParser(elements);
+				JsonNode jnode = mapper.readTree(parser);
+
+				System.out.println("-----------------------------------------");
+				System.out.println("-username : " + jnode.get("username"));
+				System.out.println("-password : " + jnode.get("password"));
+				System.out.println("-----------------------------------------");
+
+				String uname = jnode.get("username").toString().replaceAll("\"", "");
+				String upass = jnode.get("password").toString().replaceAll("\"", "");
+
+				User user = userService.getUserByUsernamePassword(uname, upass);
+
+				if (user.isEmpty()) {
+
+					mresp.setState("clear");
+					mresp.setStatus("error");
+					mresp.setMessage("this is error message");
+					mresp.setTimestamp(sdf.format(new Date()));
+				} else {
+					mresp.setState("enc");
+					mresp.setStatus("success");
+					mresp.setMessage("this is  message");
+					mresp.setTimestamp(sdf.format(new Date()));
+					mresp.setElements(Base64.getEncoder().encodeToString(elements.getBytes()));
+				}
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else {
+
+			String elements = jn.get("elements").toString().replaceAll("\\\\", "").replaceAll("^\"(.*)\"$", "$1");
+
+			System.out.println(elements.replaceAll("^\"(.*)\"$", "$1"));
+
+			ObjectMapper mapper = new ObjectMapper();
+			JsonFactory factory = mapper.getFactory();
+			JsonParser parser;
+			try {
+				parser = factory.createParser(elements);
+				JsonNode jnode = mapper.readTree(parser);
+
+				System.out.println("-----------------------------------------");
+				System.out.println("-username : " + jnode.get("username"));
+				System.out.println("-password : " + jnode.get("password"));
+				System.out.println("-----------------------------------------");
+
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		System.out.println("-----------------------------------------");
+
 		ModelAndView modelAndView = new ModelAndView();
 		// modelAndView.addObject("username", user.getUsername());
 		// modelAndView.addObject("password", user.getPassword());
-		
-		User user = repository.findById(2);
-		
-		modelAndView.addObject(user);
+
+		modelAndView.addObject(mresp);
 		modelAndView.setViewName("index");
-		return modelAndView;
+		return mresp;
 	}
 
 	@RequestMapping(value = { "/subscribe" }, method = RequestMethod.POST)
--- src/main/resources/static/js/model/login.js
+++ src/main/resources/static/js/model/login.js
@@ -5,13 +5,26 @@ class GRVLogin{
 		Survey.StylesManager.applyTheme("bootstrap");
 		var loginForm = new Survey.Model(object);
 		loginForm.showQuestionNumbers = 'off';
-		loginForm.showCompletedPage = false;
+		loginForm.showCompletedPage = true;
 		loginForm.requiredText = ''; 
 		$(this.widget).Survey({model:loginForm,onComplete:this.sendDataToServer});
 	}
 	 sendDataToServer(survey){
-		var resultAsString = JSON.stringify(survey.data);
-		alert(resultAsString); //send Ajax request to your web server. 
+		//var resultAsString = JSON.stringify(survey.data);
+		//alert(resultAsString);
+		var mr = new GRVMessageRequest(survey.data,true);
+		console.log(mr);
+		var xhr = new XMLHttpRequest();
+	    xhr.open("POST", "http://localhost:8090/login", true);
+	    xhr.setRequestHeader("Content-Type", "application/json");
+	    var dataStringify = JSON.stringify(mr);
+	    xhr.onreadystatechange = function () {
+	        if (xhr.readyState === 4 && xhr.status === 200) {
+	            var json = JSON.parse(xhr.responseText);
+	            console.log(json);
+	        }
+	    };
+	    xhr.send(dataStringify);
 	 }
 }
 
